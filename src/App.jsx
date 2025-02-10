73cc0318ee54b58d77af8db9aaffa3dd3bcab802
prem <premkumarv08@gmail.com>
2025-02-10 06:11:58 +0530

    Implementation of router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
-function App() {
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Body from "./Body";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
 
+function App() {
   return (
-    <div className="text-3xl bg-amber-200 text-black">Hello !</div>
-  )
+    <BrowserRouter basename="/" >
+      <Routes>
+        <Route path="/" element={<Body />}>
+          <Route path="/login" element={<Login />} />
+          <Route path="/profile" element={<Profile />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
